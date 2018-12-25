d94c3686a013e5adc0367cde9de39d48a9e941f8
yoshonabee <jhann0120@gmail.com>
2018-12-25 14:57:16 +0800

    model_modigy

--- model.py
+++ model.py
@@ -15,14 +15,17 @@ from torch.utils.data import Dataset, DataLoader
 class DQN(nn.Module):
     def __init__(self):
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 8, 3, stride=1, padding=2)
-        self.maxp1 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(8, 16, 3, stride=1, padding=1)
-        self.maxp2 = nn.MaxPool2d(2, 2)
-        self.conv3 = nn.Conv2d(16, 32, 3, stride=1, padding=1)
-        self.maxp3 = nn.MaxPool2d(2, 2)
+        self.conv1 = nn.Conv2d(3, 8, 5, stride=2)
+        self.bn1 = nn.BatchNorm2d(8)
+        # self.maxp1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(8, 16, 3, stride=1)
+        self.bn2 = nn.BatchNorm2d(16)
+        # self.maxp2 = nn.MaxPool2d(2, 2)
+        self.conv3 = nn.Conv2d(16, 32, 3, stride=1)
+        self.bn3 = nn.BatchNorm2d(32)
+        # self.maxp3 = nn.MaxPool2d(2, 2)
 
-        self.linear = nn.Linear(1536, 5)
+        self.linear = nn.Linear(9600, 5)
 
     def forward(self, states):
         #shape of states: (batch, state_num, agent, 3, height, width)
@@ -40,8 +43,8 @@ class DQN(nn.Module):
         return scores
 
     def conv(self, s):
-        x = F.relu(self.maxp1(self.conv1(s)))
-        x = F.relu(self.maxp2(self.conv2(x)))
-        x = F.relu(self.maxp3(self.conv3(x)))
+        x = F.relu(self.bn1(self.conv1(s)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
 
         return x.view(x.size(0), -1)
