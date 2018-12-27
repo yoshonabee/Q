dd27520dd728a098056a8c3fc4304288fff54b0b
yoshonabee <jhann0120@gmail.com>
2018-12-28 01:32:24 +0800

    play_cuda

--- play.py
+++ play.py
@@ -14,7 +14,7 @@ args = get_args()
 if args.cuda != 'default':
     os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda
     cuda = True
-    
+
 model = DQN(cuda)
 agent_num = int(args.agent_num)
 height = int(args.height)
@@ -22,7 +22,7 @@ width = int(args.width)
 
 model.load_state_dict(torch.load(args.model_path, map_location='cpu'))
 
-buff = ReplayBuffer(agent_num, height, width, DQN(), 'model/qq.pkl', 128, False)
+buff = ReplayBuffer(agent_num, height, width, DQN(cuda), 'model/qq.pkl', 128, False)
 
 buff.play(model, 128)
 # buff.save_state('./')
