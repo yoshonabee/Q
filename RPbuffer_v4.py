c0c1a808ade058ed0dfd3b3a954ecc22d189550a
yoshonabee <jhann0120@gmail.com>
2018-12-28 15:17:52 +0800

    autoencoder

---
+++ RPbuffer_v4.py
@@ -0,0 +1,159 @@
+import os
+import sys
+import datetime
+import math
+import numpy as np
+import torch
+import random
+from utils import *
+from model import *
+from lib.game.game import Game, Command
+from lib.game.agent import Agent
+from time import sleep
+import matplotlib.pyplot as plt
+#--------------------------------------------------------------------------------
+#acting.py is the class for collect the training data from the game
+#--------------------------------------------------------------------------------
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+STEPS_DONE = 0
+
+class Data:
+    def __init__(self, states, action, next_state):
+        self.states = states
+        self.action = action
+        self.next_state = next_state
+
+class ReplayBuffer():
+    def __init__(self, agent_num, height, width, model, modelpath, game_round, cuda=True):
+        #initial the model in the replay buffer
+        #model is be defined using pytorch lib
+
+        #the game is the enviroment of route planning game in the project
+        self.height = height
+        self.width = width
+        self.game_round = game_round
+        self.agent_num = agent_num
+        self.model = model
+        self.modelpath = modelpath
+        self.cuda = cuda
+
+        self.state = torch.zeros([self.agent_num, 3, self.height, self.width], dtype=torch.float32)
+
+        #default score setting in the game
+        self.acquisition_sum = 400
+        self.explored_target_sum = 70
+        self.explored_sum = 40
+        self.total_score = self.acquisition_sum + self.explored_target_sum + self.explored_sum
+        self.time_decrease = -0.00005
+        self.crash_time_penalty = -0.0001
+        self.crash_sum = -400
+        self.reg_val = 1
+
+        # self.action = torch.tensor([random.randint(0, 4) for i in range(self.agent_num)])
+        self.memory = []
+        self.buffer_limit = 1000
+
+        self.game = None
+        self.score = None
+        self.initialGame()
+
+    def collect(self, model, verbose=1):
+        if self.cuda:
+            self.model.cuda()
+
+        self.model.load_state_dict(model.state_dict())
+
+        if self.game.active and self.game.state < self.game_round:
+            #observe part of the bot
+            action = self.select_action()
+
+            self.game.runOneRound([intoCommand(i, action[i]) for i in range(self.agent_num)])
+
+            next_state = np.array([self.game.outputAgentImage(i) for i in range(self.agent_num)]).astype(np.float32)
+            next_state = torch.from_numpy(next_state)
+
+            if verbose == 1:
+                print(self.game.outputScore())
+
+            done = False
+            if self.game.active is False:
+                done = True
+
+            data = Data(self.state, action, next_state)
+            self.memory.append(data)
+
+            self.state = next_state
+
+            if len(self.memory) > self.buffer_limit:
+                self.memory.remove(self.memory[0])
+        else:
+            #only occur when the game is not active
+            self.initialGame()
+
+    
+    def select_action(self):
+        return torch.tensor([random.randint(0, 4) for i in range(self.agent_num)])
+
+
+    def save_state(self, path):
+        for i in range(self.agent_num):
+            img = np.zeros([32, 32, 3], dtype=np.uint8)
+            arr = self.game.outputAgentImage(i)
+            img[:,:,0] = arr[0,:,:]
+            img[:,:,1] = arr[1,:,:]
+            img[:,:,2] = arr[2,:,:]
+            plt.subplot(2,2,i + 1)
+            plt.imshow(img)
+        plt.show()
+
+    def play(self, model, game_round):
+        while self.game.active and self.game.state < game_round:
+            self.states[0] = self.states[1]
+            self.states[1] = self.states[2]
+            self.states[2] = self.states[3]
+            s = np.array([self.game.outputAgentImage(i) for i in range(self.agent_num)]).astype(np.float32)
+            s = torch.from_numpy(s)
+            self.states[3] = s
+
+            action = model(self.states[1:].unsqueeze(0)).max(2)[1].view(self.agent_num)
+            print(action)
+            action = [intoCommand(i, action[i]) for i in range(self.agent_num)]
+            self.game.runOneRound(action)
+            print(self.game.outputScore())
+            sleep(0.1)
+
+    def initialGame(self):
+        #initial the game envirnment for th replay buffer
+        t = (self.height + self.width) / 2
+
+        self.game = Game(self.height, self.width, self.game_round)
+        self.game.setRandomMap(self.agent_num, int(t * 0.3) ** 2, int(t * 0.1) ** 2)
+        self.game.setScore(self.acquisition_sum, self.explored_target_sum, self.explored_sum, self.time_decrease, self.crash_time_penalty, self.crash_sum, self.reg_val)
+
+        self.game.runOneRound([Command(i, 0, 0) for i in range(self.agent_num)])
+        self.score = np.array([self.game.outputScore() for i in range(self.agent_num)])
+ 
+        self.state = np.array([self.game.outputAgentImage(i) for i in range(self.agent_num)]).astype(np.float32)
+        self.state = torch.from_numpy(self.state)
+
+        print('New Game!')
+
+    def loadWeight(self):
+        if os.path.exists(self.modelpath):
+            if self.cuda:
+                self.model.load_state_dict(torch.load(self.modelpath))
+            else:
+                self.model.load_state_dict(torch.load(self.modelpath, map_location='cpu'))
+        else:
+            print('Model weight [{0}] not found'.format(self.modelpath))
+        return
+
+
+def intoCommand(i, command):
+    if command == 0: return Command(i, 0, 1)
+    elif command == 1: return Command(i, 1, 0)
+    elif command == 2: return Command(i, 0, -1)
+    elif command == 3: return Command(i, -1, 0)
+    return Command(i, 0, 0)
---
+++ autoencoder.py
@@ -0,0 +1,90 @@
+from model import *
+from utils import get_args
+import RPbuffer_v4
+from RPbuffer_v4 import ReplayBuffer
+
+import os
+import sys
+
+import numpy as np
+import torch
+from torch.optim import RMSprop
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+
+args = get_args()
+
+keep_train = True
+cuda = False
+
+LR = 0.001
+
+ITER = 4000000
+BATCH_SIZE = 8
+TARGET_UPDATE = 10
+
+height = int(args.height)
+width = int(args.width)
+agent_num = int(args.agent_num)
+
+if args.cuda != 'default':
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda
+    cuda = True
+
+buff = ReplayBuffer(agent_num, height, width, AE(), args.model_path, 10000, cuda)
+
+model = AE()
+embed = Embedding()
+criterion = nn.MSELoss()
+optim = RMSprop(model.parameters(), lr=LR)
+
+if args.keep_train != 'default':
+    print('keep training, modelpath: {0}'.format(args.model_path))
+    model.load_state_dict(torch.load(args.model_path))
+
+if cuda:
+    model.cuda()
+    embed.cuda()
+    criterion.cuda()
+
+action = torch.zeros([BATCH_SIZE, agent_num])
+
+for i in range(ITER):
+    buff.collect(model, 0)
+
+    if len(buff.memory) < BATCH_SIZE: continue
+
+    batch = [random.choice(buff.memory) for j in range(BATCH_SIZE)]
+
+    
+
+    states = torch.cat([batch[j].states.unsqueeze(0) for j in range(BATCH_SIZE)])
+    next_states = torch.cat([batch[j].next_state.unsqueeze(0) for j in range(BATCH_SIZE)])
+    action = torch.cat([batch[j].action.unsqueeze(0) for j in range(BATCH_SIZE)])
+
+    if cuda:
+        states = states.cuda()
+        action = action.cuda()
+
+    action = action.long().view(BATCH_SIZE, agent_num, 1)
+
+    state_vec = model(states)
+    next_state_vec = model(next_states)
+
+    action_vec = embed(action)
+    state_vec  = state_vec + action_vec
+
+    
+
+    loss = criterion(state_vec, next_state_vec)
+    optim.zero_grad()
+    loss.backward()
+    # for param in model.parameters():
+    #     param.grad.data.clamp_(-1, 1)
+    optim.step()
+
+    if (i + 1) % 100 == 0:
+        print('Iter:%d | loss:%.4f' %(i + 1, loss.item()))
+    
+    if (i + 1) % 100 == 0:
+        torch.save(model.state_dict(), args.model_path)
